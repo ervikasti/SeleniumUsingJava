1add9658f7d06967383087ce4bd6b237b9d1954f
Vikas TIwari <30732434+ervikasti@users.noreply.github.com>
2020-12-16 20:22:50 +0530

    Github login using Selenium

--- Github2.java
+++ Github2.java
@@ -1,17 +1,26 @@
 package com.company;
 // Here we are going to find elements using CSS Selector
+import java.util.Scanner;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class Github2 {
     public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
     System.setProperty("webdriver.chrome.driver","C:\\Users\\draka\\Downloads\\chromedriver_win32\\chromedriver.exe");
     WebDriver driver = new ChromeDriver();
     driver.navigate().to("https://github.com/");
     //Clicking on Sign in
         driver.findElement(By.linkText("Sign in")).click();
-   //Below is error
-        // driver.findElement(By.cssSelector("input[name]")).sendKeys("ervikasti@gmail.com");
+   //Click on username
+        driver.findElement(By.name("login")).sendKeys("gmail");
+   //Click on password
+        //System.out.print("Enter the Password : ");
+        driver.findElement(By.name("password")).sendKeys("paswwd");
+    //Click on Login
+        driver.findElement(By.name("commit")).click();
+
 
     //driver.close();
 
